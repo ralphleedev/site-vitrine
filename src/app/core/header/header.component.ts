1ff2b0aaf6f3abef5d385a4931be41a02628c13a
Ralph Lee <130049567+ralphleedev@users.noreply.github.com>
2026-08-19 16:03:49 +0200

    feat: add responsive header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,9 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+}
