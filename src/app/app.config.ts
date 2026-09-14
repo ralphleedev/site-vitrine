280f0020ae7ef4a74bdfabdee9a5be5b16d5af9f
Ralph Lee <130049567+ralphleedev@users.noreply.github.com>
2026-09-14 15:02:03 +0200

    fix(i18n): use relative path for translation loader prefix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideTranslateService({
-      loader: provideTranslateHttpLoader({ prefix: '/i18n/', suffix: '.json' }),
+      loader: provideTranslateHttpLoader({ prefix: 'i18n/', suffix: '.json' }),
     }),
   ]
 };
