25bc322ce95b85b1590e7808dd6ff0593ae0b372
Ralph Lee <130049567+ralphleedev@users.noreply.github.com>
2026-09-14 14:34:51 +0200

    feat: set up app routing and page components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
-    {
+  {
     path: '',
     component: MainLayoutComponent,
     children: [
-      { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) }
+      { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+      { path: 'about', loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
+      { path: 'projects', loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent) },
+      { path: 'contact', loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent) },
+      { path: '**', redirectTo: '' }
     ]
   }
 ];
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { Language } from '../../shared/enums/language.enums';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, TranslatePipe],
+  imports: [CommonModule, TranslatePipe, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
